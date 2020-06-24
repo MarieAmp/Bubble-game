15167349b0330128fea6b7103826d658c72cd635
MarieAmp <marymary09@hotmail.com>
2020-06-24 15:55:41 +0200

    collision ok

--- javascript/newfish.js
+++ javascript/newfish.js
@@ -8,81 +8,121 @@ var fish = new Image();
 fish.src = "../images/fish-2638627_640.png";
 
 var leftB = new Image();
-leftB.src = "../images/brancheG.png";
+leftB.src = "../images/block test.png";
 
 var rightB = new Image();
-rightB.src = "../images/brancheD.png";
+rightB.src = "../images/block test2.png";
 
 var fishX = 300;
 var fishY = 650;
 
 document.addEventListener("keydown", (e) => {
-    switch (e.keyCode) {
-      case 38: // up arrow
-        fishY -= 10;
-        break;
-      case 40: // down arrow
-        fishY += 10;
-        break;
-      case 37: // left arrow
-        fishX -= 10;
-        break;
-      case 39: // right arrow
-        fishX += 10;
-        break;
-    }
-  });
-  
-// let x = canvas.width;
-// let minWidth = 100;
-// let maxWidth = 300;
-// let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-// let minGap = 200;
-// let maxGap = 400;
-// let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+  switch (e.keyCode) {
+    case 38: // up arrow
+      fishY -= 20;
+      break;
+    case 40: // down arrow
+      fishY += 20;
+      break;
+    case 37: // left arrow
+      fishX -= 20;
+      break;
+    case 39: // right arrow
+      fishX += 20;
+      break;
+  }
+});
 
-let gap = 250;
-let constant = leftB.width + gap;
+let gap = 200;
+let ajust = leftB.width + gap;
 
-//gameover obstacles
+var obstacles = [];
 
-
-
-var obstacles=[];
-
-obstacles[0]={
-    x:0,
-    y:0
+obstacles[0] = {
+  x: 0,
+  y: 0,
 };
 
 function draw() {
+  var fishHeight = 150;
+  var fishWidth = 150;
   ctx.drawImage(bkg, 0, 0);
-  ctx.drawImage(fish, fishX,fishY,150,150);
+  ctx.drawImage(fish, fishX, fishY, 150, 150);
 
-  for(let i=0;i<obstacles.length;i++){
-  ctx.drawImage(leftB, obstacles[i].x,obstacles[i].y,);
-  ctx.drawImage(rightB, obstacles[i].x+constant, obstacles[i].y,);
+  for (let i = 0; i < obstacles.length; i++) {
+    ctx.drawImage(leftB, obstacles[i].x, obstacles[i].y);
+    ctx.drawImage(rightB, obstacles[i].x + ajust, obstacles[i].y);
 
-  obstacles[i].y++; 
+    obstacles[i].y++;
 
-  if(obstacles[i].y===400){
+    if (obstacles[i].y == 500) {
       obstacles.push({
-          x:Math.floor(Math.random() * (leftB.width - rightB.width+1)),
-          y:0
+        x: Math.floor(Math.random() * leftB.width) - leftB.width,
+        y: 0,
       });
-  }
+    }
 
-  } 
+    if (
+      fishY <= obstacles[i].y + leftB.height &&
+      //fishY + fishHeight <= obstacles[i].y + leftB.height &&
+      (fishX <= obstacles[i].x + leftB.width || fishX + fishWidth >= obstacles[i].x + ajust)
+    ) {
+    console.log("collision");
+    
+    }
+  }
   requestAnimationFrame(draw);
 }
 
 draw();
 
-function crashWith(obstacles) {
-    return !(
-      fish.bottom() < obstacles[i].top() ||
-      fish.top() > obstacle[i].bottom() ||
-      fish.right() < obstacle[i].left() ||
-      fish.left() > obstacle[i].right()
-    );
-  }
+// function checkCollisions(allObstacles){
+//   for (let i=0; i<allObstacles.length; i++){
+//       if (player.x < allObstacles[i].x + allObstacles[i].w &&
+//           player.x + 120 > allObstacles[i].x &&
+//           player.y < allObstacles[i].y + allObstacles[i].h &&
+//           80 + player.y > allObstacles[i].y) {
+//            console.log('collision')
+//            player.score += 10
+//            console.log(player.score)
+//            var theScore = document.getElementById('thescore')
+//            theScore.innerHTML = `${player.score}`
+//        } else if (player.y < allObstacles[i].y) {
+//            player.lives -= 1
+//            console.log(player.lives)
+//        }
+//   }
+// // }
+// function checkCollisions(allObstacles){
+//     for (let i=0; i<allObstacles.length; i++){
+// if (
+//   (fishX < obstacles[i].x + obstacles[i].width &&
+//     (fishY < obstacles[i].y + obstacles[i].height ||
+//     fishY> obstacles[i].y))
+//   //   ||
+//   // (fishX + 150 > obstacles[i].x+ obstacles[i].width+ ajust &&
+//   //   fishY < obstacles[i].y + obstacles[i].height &&
+//   //   fishY>obstacles[i].y)
+
+// ) {
+//   window.alert("collision");
+// }
+//     }
+//   }
+
+const backgroundImage = {
+  img: img,
+  x: 0,
+  y: 0,
+  speed: -1,
+
+  move: function () {
+    this.y += this.speed;
+    this.y %= backgroundCanvas.height;
+  },
+};
+
+// fishY<= obstacles[i].y +leftB.height &&
+//  fishY + fish.height <= obstacles[i].y + leftB.height&&
+//  (fishX<=obstacles[i]+leftB.width ||
+//   fishX+150>=leftB+ajust)
